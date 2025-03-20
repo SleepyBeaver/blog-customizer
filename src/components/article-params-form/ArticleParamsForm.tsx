6881fe8124bdabf5771381ca6a38a6b774d470c7
Kuznetsov Dmitry <dima.mr-smith@yandex.ru>
2025-03-20 16:30:24 +0300

    add open/close form mechanics

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,38 @@
+import { useRef, useState, useCallback } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+
+import { useClose } from '../hooks/useClose';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
-	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
-		</>
-	);
+    const ref = useRef<HTMLFormElement | null>(null)
+    const [flag, setFlag] = useState(false);
+
+    useClose({
+        open: flag,
+        onClose: () => setFlag(false),
+        rootRef: ref
+    })
+
+    const toggleFlag = useCallback(() => {
+        setFlag(prevFlag => !prevFlag);
+    },[])
+
+  return (
+    <>
+      <ArrowButton onClick={toggleFlag} isOpen={flag}/>
+      <aside className = {clsx(styles.container, {[styles.container_open] : flag})}>
+        <form className={styles.form}>
+          <div className={styles.bottomContainer}>
+            <Button title='Сбросить' htmlType='reset' type='clear' />
+            <Button title='Применить' htmlType='submit' type='apply' />
+          </div>
+        </form>
+      </aside>
+    </>
+  );
 };
---
+++ src/components/hooks/useClose.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useEffect } from "react";
+
+type useCloseProps = {
+    open: boolean;
+    onClose: () => void;
+    rootRef: React.RefObject<HTMLElement>;
+};
+
+export function useClose({open, onClose, rootRef}: useCloseProps) {
+    const eventClickOutside = useCallback((event: MouseEvent) => {
+        if (event instanceof Node && rootRef.current && !rootRef.current.contains(event)) {
+            onClose();
+        }
+    }, [onClose, rootRef]);
+
+    const eventKeyEscape = useCallback((event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+            onClose();
+        }
+    }, [onClose]);
+
+    useEffect(() => {
+        if (!open) return;
+
+        document.addEventListener("keydown", eventKeyEscape);
+        document.addEventListener("mousedown", eventClickOutside);
+
+        return () => {
+            document.removeEventListener("keydown", eventKeyEscape);
+            document.removeEventListener("mousedown", eventClickOutside);
+        };
+
+    }, [open, eventKeyEscape, eventClickOutside]);
+}
