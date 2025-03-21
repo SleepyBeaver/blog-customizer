1bee6e2656c41acb19e0807cb71ab13c9261f49b
Kuznetsov Dmitry <dima.mr-smith@yandex.ru>
2025-03-21 17:03:32 +0300

    add editing mechanics ArticleState

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,40 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, FormEvent } from 'react';
 import clsx from 'clsx';
 
+import { OptionType, ArticleStateType, fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr } from 'src/constants/articleProps';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
-import { useClose } from '../hooks/useClose';
+import { Text } from "src/ui/text";
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import { useClose } from '../hooks/useClose';
+
+
+type ArticleParamsFormProps = {
+  fontFamily: (select: OptionType) => void,
+  fontSize: (select: OptionType) => void,
+  fontColor: (select: OptionType) => void,
+  backgroundColor: (select: OptionType) => void,
+  contentWidth: (select: OptionType) => void,
+  resetButton: () => void,
+  applyButton: (event: FormEvent) => void,
+  paramsFormState: ArticleStateType,
+}
+
+export const ArticleParamsForm = ({
+  fontFamily,
+  fontSize,
+  fontColor,
+  backgroundColor,
+  contentWidth,
+  resetButton,
+  applyButton,
+  paramsFormState
+}: ArticleParamsFormProps) => {
     const ref = useRef<HTMLFormElement | null>(null)
     const [flag, setFlag] = useState(false);
 
@@ -26,9 +52,16 @@ export const ArticleParamsForm = () => {
     <>
       <ArrowButton onClick={toggleFlag} isOpen={flag}/>
       <aside className = {clsx(styles.container, {[styles.container_open] : flag})}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit = {applyButton}>
+          <Text size = {31} weight = {800} uppercase as = {'h3'}>Задайте параметры</Text>
+          <Select selected = {paramsFormState.fontFamilyOption} options = {fontFamilyOptions} onChange = {fontFamily} title = 'Шрифт'/>
+          <RadioGroup name = 'fontSize' options = {fontSizeOptions} selected = {paramsFormState.fontSizeOption} onChange = {fontSize} title = 'Font size' />
+          <Select selected = {paramsFormState.fontColor} options = {fontColors} onChange = {fontColor} title = 'Font color' />
+          <Separator/>
+          <Select selected = {paramsFormState.backgroundColor} options = {backgroundColors} onChange = {backgroundColor} title = 'Backgorund color' />
+          <Select selected = {paramsFormState.contentWidth} options = {contentWidthArr} onChange = {contentWidth} title = 'Content width' />
           <div className={styles.bottomContainer}>
-            <Button title='Сбросить' htmlType='reset' type='clear' />
+            <Button title='Сбросить' htmlType='reset' type='clear' onClick = {resetButton}/>
             <Button title='Применить' htmlType='submit' type='apply' />
           </div>
         </form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, FormEvent } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, ArticleStateType, OptionType } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +13,60 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [paramsFormState, setParamsFormState] = useState<ArticleStateType>(defaultArticleState)
+	const [articleState, setArticleState] = useState(defaultArticleState)
+
+	const setFontFamily = (select: OptionType) => {
+		setParamsFormState({...paramsFormState, fontFamilyOption: select})
+	}
+
+	const setFontSize = (select: OptionType) => {
+		setParamsFormState({...paramsFormState, fontSizeOption: select})
+	}
+
+	const setFontColor = (select: OptionType) => {
+		setParamsFormState({...paramsFormState, fontColor: select})
+	}
+
+	const setBackgroundColor = (select: OptionType) => {
+		setParamsFormState({...paramsFormState, backgroundColor: select})
+	}
+
+	const setContainerWidth = (select: OptionType) => {
+		setParamsFormState({...paramsFormState, contentWidth: select})
+	}
+
+	const resetParamsFormState = () => {
+		setArticleState(defaultArticleState);
+		setParamsFormState(defaultArticleState)
+	}
+
+	const applyParamsFormState = (event: FormEvent) => {
+		event.preventDefault();
+		setArticleState(paramsFormState);
+	}
+	
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm 
+			fontFamily = {setFontFamily} 
+			fontSize = {setFontSize} 
+			fontColor = {setFontColor}
+			backgroundColor = {setBackgroundColor}
+			contentWidth = {setContainerWidth} 
+			resetButton = {resetParamsFormState}
+			applyButton = {applyParamsFormState}
+			paramsFormState = {paramsFormState} />
 			<Article />
 		</main>
 	);
